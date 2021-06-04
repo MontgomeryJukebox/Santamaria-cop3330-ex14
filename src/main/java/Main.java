a80440d3d988c00c5f91cc5e2e5d70ff06e9fe44
David Santamaria <davidsantamaria1245@gmail.com>
2021-06-03 23:41:31 -0400

    Upload files

---
+++ src/main/java/Main.java
@@ -0,0 +1,33 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 David Santamaria
+ */
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 David Santamaria
+ */
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What is the order amount?");
+        int n = in.nextInt();
+        System.out.print("What is the state?");
+        String state = in.nextLine();
+        double total =
+        if (state.equals("WI")) {
+
+        }
+        System.out.printf("The total is $.2f.\n", total);
+    }
+}
