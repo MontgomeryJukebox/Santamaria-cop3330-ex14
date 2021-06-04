e78e7022acd467af0ca3e8dee3fbc192d5bb6b8c
David Santamaria <davidsantamaria1245@gmail.com>
2021-06-03 23:49:54 -0400

    Upload files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,28 +6,23 @@
  *  UCF COP3330 Summer 2021 Assignment 1 Solution
  *  Copyright 2021 David Santamaria
  */
-/*
- *  UCF COP3330 Summer 2021 Assignment 1 Solution
- *  Copyright 2021 first_name last_name
- */
-/*
- *  UCF COP3330 Summer 2021 Assignment 1 Solution
- *  Copyright 2021 first_name last_name
- */
 import java.util.Scanner;
 
 public class Main {
+    public static final double TAX = 0.055;
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         System.out.print("What is the order amount?");
         int n = in.nextInt();
+        in.nextLine();
         System.out.print("What is the state?");
         String state = in.nextLine();
-        double total =
+        double total = n;
         if (state.equals("WI")) {
-
+            System.out.printf("The tax is $%.2f\n", total * TAX);
+            total += total * TAX;
         }
-        System.out.printf("The total is $.2f.\n", total);
+        System.out.printf("The total is $%.2f.\n", total);
     }
 }
